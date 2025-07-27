8dfc883991566321ef79ecd028018a6c8571dba6
Caner Özdemir <canerozdemir489@gmail.com>
2025-07-27 18:01:18 +0300

    Implemented card drag input.

--- Assets/_Game/Scripts/Systems/GameInputSystem.cs
+++ Assets/_Game/Scripts/Systems/GameInputSystem.cs
@@ -12,6 +12,10 @@ namespace _Game.Scripts.Systems
     {
         private GameInput _gameInput;
         private Transform draggingCard;
+        private bool _isDragging;
+        private Camera _mainCamera;
+        
+        private Vector3 _dragOffset;
         
         public void Initialize()
         {
@@ -19,11 +23,17 @@ namespace _Game.Scripts.Systems
             _gameInput.Enable();
             _gameInput.Card.PointerPress.started += OnCardPointerPressed;
             _gameInput.Card.PointerPress.canceled += OnCardPointerPressStopped;
+            _mainCamera = Camera.main;
         }
         
         public void Tick()
         {
-            
+            if (_isDragging && draggingCard != null)
+            {
+                Vector2 screenPos = _gameInput.Card.PointerPosition.ReadValue<Vector2>();
+                Vector3 worldPos = _mainCamera.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, _mainCamera.nearClipPlane)); // 5f is distance from camera
+                draggingCard.position = new Vector3(worldPos.x, worldPos.y, draggingCard.position.z);
+            }
         }
 
         public void Dispose()
@@ -35,11 +45,32 @@ namespace _Game.Scripts.Systems
         
         private void OnCardPointerPressed(InputAction.CallbackContext obj)
         {
-            
+            Vector2 screenPos = _gameInput.Card.PointerPosition.ReadValue<Vector2>();
+            Vector3 worldPos = _mainCamera.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, _mainCamera.nearClipPlane));
+            RaycastHit2D hit = Physics2D.Raycast(worldPos, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                draggingCard = hit.collider.transform;
+                _isDragging = true;
+                _dragOffset = draggingCard.position - worldPos;
+            }
         }
         
         private void OnCardPointerPressStopped(InputAction.CallbackContext obj)
         {
+            if (!_isDragging || draggingCard == null)
+                return;
+
+            Vector2 screenPos = _gameInput.Card.PointerPosition.ReadValue<Vector2>();
+            Vector3 worldPos = _mainCamera.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, _mainCamera.nearClipPlane));
+            // RaycastHit2D hit = Physics2D.Raycast(worldPos, Vector2.zero);
+
+            draggingCard.position = new Vector3(worldPos.x, worldPos.y, draggingCard.position.z);
+
+
+            _isDragging = false;
+            draggingCard = null;
             
         }
     }
